cd3a12448432cb05b75ba27d0d71de6e6e0881c3
Leonardo Ferreyra <leferreyra@gmail.com>
2013-08-28 11:05:03 -0300

    Adds Background panel, MenuBar and ToolBar

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 import wx
 from views import MainFrame
@@ -6,14 +7,100 @@ from views import MainFrame
 
 
 
-class App(wx.App):
+class MainController:
 
+
+	def __init__(self, app, main_frame):
+
+		self.app = app
+		self.main_frame = main_frame
+
+		# setting menubar and toolbar data
+		self.main_frame.createMenuBar(self.getMenuData())
+		self.main_frame.createToolBar(self.getToolbarData())
+
+
+	def getMenuData(self):
+
+		# defining menus and it's callbacks methods
+		return (
+			("&Archivo", 
+				("&Hacer Backup", "Hacer archivo de backup", self.OnBackup),
+				("&Restaurar Backup", "Restaurar datos desde un backup", self.OnRestoreBackup),
+				("", "", ""), # Add separator
+				("&Configuración...", "Opciones varias de configuración", self.OnConfig),
+				("", "", ""), # Add separator
+				("Salir", "Salir de la aplicación", self.OnExit),
+			),
+			("&Clientes", 
+				("&Nuevo Cliente...", "Crear nuevo cliente", self.OnNewClient),
+				("", "", ""), # Add separator
+				("&Administrar clientes...", "Administración de clientes", self.OnClients),
+			),
+			("&Gestión", 
+				("&Comprar/Vender...", "Realizar transacción", self.OnNewTransaction),
+				("", "", ""), # Add separator
+				("&Administrar Stock...", "Stock de Divisas", self.OnAdminStock),
+				("C&heques...", "Salir de la aplicación", self.OnChecks),
+			),
+		)
+
+
+	def getToolbarData(self):
+
+		return (
+			("Nuevo cliente", "Crear nuevo cliente", "images/silver/test.png", self.OnNewClient),
+			("Administrar clientes", "Administrar clientes", "images/silver/test.png", self.OnClients),
+			("", "", "", None)
+		)
+
+
+	def OnBackup(self, event):
+		pass
+
+
+	def OnRestoreBackup(self, event):
+		pass
+
+		
+	def OnConfig(self, event):
+		pass
+
+
+	def OnExit(self, event):
+		pass
+
+
+	def OnNewClient(self, event):
+		pass
+
+
+	def OnClients(self, event):
+		pass
+
+
+	def OnNewTransaction(self, event):
+		pass
+
+
+	def OnAdminStock(self, event):
+		pass
+
+
+	def OnChecks(self, event):
+		pass
+
+
+
+class App(wx.App):
 
 
 	def OnInit(self):
 
 		self.main_frame = MainFrame()
 		self.main_frame.Show()
+
+		self.main_controller = MainController(self, self.main_frame)
 
 		self.SetTopWindow(self.main_frame)
 
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 import wx
 
@@ -11,4 +12,131 @@ class MainFrame(wx.Frame):
 	def __init__(self, parent=None, ID=-1):
 
 		wx.Frame.__init__(self, parent, ID, size=(900, 600), 
-			pos=wx.DefaultPosition, style=wx.DEFAULT_FRAME_STYLE | wx.MAXIMIZE)
+			pos=wx.DefaultPosition, style=wx.DEFAULT_FRAME_STYLE )
+
+		self.panel = BackgroundPanel(self, -1)
+		self.panel.SetBackgroundColour("black")
+
+		self.SetTitle("Gestión")
+		self.Center()
+
+		self.initUi()
+		self.makeLayout()
+
+
+	def makeLayout(self):
+		pass
+
+
+	def initUi(self):
+		pass
+
+
+	def createToolBar(self, toolBarData):
+
+		toolbar = self.CreateToolBar(style=wx.TB_VERTICAL | wx.TB_NODIVIDER )
+		toolbar.SetBackgroundColour("#333333")
+
+		for eachToolData in toolBarData:
+			self.createTool(toolbar, *eachToolData)
+
+		toolbar.Realize()
+
+
+	def createTool(self, toolbar, label, help, filename, handler):
+
+		if not label:
+			toolbar.AddSeparator()
+			return
+
+		tool = toolbar.AddSimpleTool(-1, wx.Bitmap(filename), label, help)
+		self.Bind(wx.EVT_MENU, handler, tool)
+
+
+	def createMenuBar(self, menuBarData):
+
+		menuBar = wx.MenuBar()
+
+		for eachMenuData in menuBarData:
+			menuLabel = eachMenuData[0]
+			menuItems = eachMenuData[1:]
+			menuBar.Append(self.createMenu(menuItems), menuLabel)
+
+		self.SetMenuBar(menuBar)
+
+
+	def createMenu(self, menuData):
+
+		menu = wx.Menu()
+
+		for label, status, handler in menuData:
+			if not label:
+				menu.AppendSeparator()
+				continue
+			menuItem = menu.Append(-1, label, status)
+			self.Bind(wx.EVT_MENU, handler, menuItem)
+
+		return menu
+
+
+
+
+# =====================
+# Some Custom wxWindows
+# =====================
+
+class BackgroundPanel(wx.Panel):
+
+
+	def __init__(self, parent=None, ID=-1):
+
+		wx.Panel.__init__(self, parent, ID)
+
+		self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
+		self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
+		self.Bind(wx.EVT_SIZE, self.OnSize)
+		self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+
+	def OnPaint(self, event):
+
+		dc = wx.BufferedPaintDC(self, self.buffer)
+
+
+	def OnSize(self, event):
+
+		w, h = self.GetClientSize()
+		self.buffer = wx.EmptyBitmap(w, h)
+		dc = wx.BufferedDC(wx.ClientDC(self), self.buffer)
+
+		self.Draw(dc)
+
+		self.Refresh()
+
+
+	def OnEraseBackground(self, event):
+
+		# Vacio intencionalemente
+		pass
+
+
+	def Draw(self, dc):
+
+		w, h = self.GetClientSize()
+		dc.Clear()
+
+		bmp = wx.Bitmap("images/wood.png")
+		bmp_logo = wx.Bitmap("images/w3bex-logo.png")
+
+		lw, lh = bmp_logo.GetSize()
+		bw, bh = bmp.GetSize()
+
+		# Centrar la imagen
+		xpos = (w - bw) / 2
+		ypos = (h - bh) / 2
+
+		logox = w - lw
+		logoy = h - lh
+
+		dc.DrawBitmap(bmp, xpos, ypos)
+		dc.DrawBitmap(bmp_logo, logox, logoy)
